ded092f5c55d66d3434d834497818bb492eb7216
Ali SLAOUI <ali.slaoui@hcm-cp.com>
2024-12-15 23:34:22 +0100

    update paths

--- main.py
+++ main.py
@@ -1017,7 +1017,7 @@ def extract_hierarchy_from_zip(zip_buffer):
                     previous_value = row[col]
         return rows
 
-    rows = remove_redundancy(rows)
+    #rows = remove_redundancy(rows)
 
     # Trouver la profondeur maximale
     max_depth = max(len(row) for row in rows)
@@ -1554,7 +1554,10 @@ if __name__ == "__main__":
                             current_task_placeholder.success("Traitement terminé avec succès !")
 
                     except Exception as e:
-                        st.write(e)
+                        import traceback
+                        current_task_placeholder.text(f"Une erreur est survenue : {e}")
+                        st.text("Traceback détaillé :")
+                        st.text(traceback.format_exc())
 
         else:
             st.markdown('<div class="feature-description bold">Importez un fichier et choisissez la méthode pour exporter et autres filtres si nécessaire.</div>', unsafe_allow_html=True)
